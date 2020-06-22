6a21ccce6903c0d466a78fe56fc849355de4ad74
unknown <snstotti@gmail.com>
2020-06-22 21:05:02 +0300

    starDB first commit

---
+++ src/service/SwapiService.js
@@ -0,0 +1,49 @@
+class SwapiService {
+    _apiBase = 'https://swapi.dev/api'
+  
+    async getResource(url) {
+      const res = await fetch(`${this._apiBase}${url}`)
+      if (!res.ok) {
+        throw new Error(`Not fetch ${url} + received ${res.status}`)
+      }
+      return await res.json();
+    }
+  
+    async getAllPeople() {
+      const res = await this.getResource('/people/');
+      return res.results
+    }
+    getPerson(id) {
+      return this.getResource(`/people/${id}/`);
+    }
+  
+    async getAllPlanets() {
+      const res = await this.getResource('/planets/');
+      return res.results
+    }
+    getPlanet(id) {
+      return this.getResource(`/planets/${id}/`);
+    }
+  
+    async getAllStarships() {
+      const res = await this.getResource('/starships/');
+      return res.results
+    }
+  
+    getStarships(id) {
+      return this.getResource(`/starships/${id}/`);
+    }
+  }
+  
+//   const swapi = new SwapiService()
+//   swapi.getStarships(4).then(p => console.log(p.name))
+//   // swapi.getAllStarships().then(people=>people.forEach(e => {
+//   //   console.log(e.name);
+  
+//   // }));
+  
+  
+  
+  
+  
+  
