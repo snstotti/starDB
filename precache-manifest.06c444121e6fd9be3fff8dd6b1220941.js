495ab18669fa2d037c850465ca28c339373f7e60
unknown <snstotti@gmail.com>
2020-11-02 14:21:58 +0300

    Updates

---
+++ precache-manifest.06c444121e6fd9be3fff8dd6b1220941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac55bee1dfafabd80b36ea88a4982e54",
+    "url": "/starDB/index.html"
+  },
+  {
+    "revision": "16a354a41cde733df222",
+    "url": "/starDB/static/css/main.4e0d2388.chunk.css"
+  },
+  {
+    "revision": "de4513170fa1f194601f",
+    "url": "/starDB/static/js/2.72364207.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/starDB/static/js/2.72364207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a354a41cde733df222",
+    "url": "/starDB/static/js/main.80fa5542.chunk.js"
+  },
+  {
+    "revision": "1bc32fcecec408043361",
+    "url": "/starDB/static/js/runtime-main.47fb1933.js"
+  }
+]);
